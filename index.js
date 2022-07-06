7b95a7ddacc5ee65b433766c3852f1f0b403bc8d
Muhammad Haroon <84071543+26haroon26@users.noreply.github.com>
2022-07-06 21:58:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function start() {
-    document.getElementById('seconddiv').style.display = "none";
+    document.getElementById('firstdiv').style.display = "block";
+    document.getElementById('calendar').style.display = "block";
+    document.getElementById('firstbtn').style.display = "none";
 }
 
 function Times() {
@@ -7,21 +9,20 @@ function Times() {
     let inp_ut1 = document.getElementById("citynames");
     let inp_ut2 = document.getElementById("countrynames");
 
-    axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${inp_ut1.value}&country=${inp_ut2.value}&method=1`)
+    axios.get(`http://api.aladhan.com/v1/timingsByCity?city=${inp_ut1.value}&country=${inp_ut2.value}&method=1`)
         .then(function (response) {
-            document.getElementById('fajr').innerHTML = response.data.data.timings.Fajr;
-            document.getElementById('sunrise').innerHTML = response.data.data.timings.Sunrise;
-            document.getElementById('zuhur').innerHTML = response.data.data.timings.Dhuhr;
-            document.getElementById('asr').innerHTML = response.data.data.timings.Asr;
-            document.getElementById('sunset').innerHTML = response.data.data.timings.Sunset;
-            document.getElementById('maghrib').innerHTML = response.data.data.timings.Maghrib;
-            document.getElementById('esha').innerHTML = response.data.data.timings.Isha;
-            document.getElementById('hijridate').innerHTML = response.data.data.date.gregorian.date + " " + response.data.data.date.gregorian.designation.abbreviated;
-            document.getElementById('gregoriandate').innerHTML = response.data.data.date.hijri.date + " " + response.data.data.date.hijri.designation.abbreviated;
+            document.getElementById('fajr').innerHTML = "Fajr :" + " " + response.data.data.timings.Fajr;
+            document.getElementById('sunrise').innerHTML = "Sunrise :" + " " + response.data.data.timings.Sunrise;
+            document.getElementById('zuhur').innerHTML = "Zuhur :" + " " + response.data.data.timings.Dhuhr;
+            document.getElementById('asr').innerHTML = "Asr :" + " " + response.data.data.timings.Asr;
+            document.getElementById('sunset').innerHTML = "Sunset :" + " " + response.data.data.timings.Sunset;
+            document.getElementById('maghrib').innerHTML = "Maghrib" + " " + response.data.data.timings.Maghrib;
+            document.getElementById('esha').innerHTML = "Esha" + " " + response.data.data.timings.Isha;
+            document.getElementById('tahajjud').innerHTML = "Tahajjud :" + " " + response.data.data.timings.Midnight;
+            document.getElementById('hijridate').innerHTML = "Hijri Date :" + " " + response.data.data.date.gregorian.date + " " + response.data.data.date.gregorian.designation.abbreviated;
+            document.getElementById('gregoriandate').innerHTML = "Gregorian Date :" + " " + response.data.data.date.hijri.date + " " + response.data.data.date.hijri.designation.abbreviated;
         }
         )
-    document.getElementById('citytimes').style.display = 'block';
-
     inp_ut1.value = "";
     inp_ut2.value = "";
 }
@@ -40,11 +41,11 @@ function HijriCalendar() {
     let inp_ut3 = document.getElementById("monthsnames");
     let inp_ut4 = document.getElementById("yearnames");
 
-    axios.get(`https://api.aladhan.com/v1/hijriCalendarByCity?city=${inp_ut1.value}&country=${inp_ut2.value}&method=1&month=${inp_ut3.value}&year=${inp_ut4.value}`)
-        .then(function (response) {
-            console.log(response.data);
-            for (let i = 0; i < response.data.data.length; i++) {
-                response.data.data[i].date.gregorian.date;
+    axios.get(`http://api.aladhan.com/v1/hijriCalendarByCity?city=${inp_ut1.value}&country=${inp_ut2.value}&method=1&month=${inp_ut3.value}&year=${inp_ut4.value}`)
+    .then(function (response) {
+        console.log(response.data);
+        for (let i = 0; i < response.data.data.length; i++) {
+            response.data.data[i].date.gregorian.date;
                 response.data.data[i].date.gregorian.weekday.en;
                 response.data.data[i].date.hijri.date;
                 response.data.data[i].timings.Fajr;
@@ -57,7 +58,6 @@ function HijriCalendar() {
                 response.data.data[i].timings.Sunset;
 
                 document.getElementById('table_for_calendar').innerHTML += `<tr>
-
                 <td>${i + 1}</td>
                 <td>${response.data.data[i].date.hijri.date}</td>
                 <td>${response.data.data[i].date.gregorian.date}</td>
@@ -89,24 +89,5 @@ function HijriCalendar() {
 
         }
         )
-    document.getElementById('form').style.display = "none";
-    document.getElementById('firstbtn').style.display = 'block';
-
-}
-// function asma_ul_husna() {
-//     axios.get(`http://api.aladhan.com/asmaAlHusna/`)
-//         .then(function (response) {
-//             console.log(response.data);
-//             let k = 0;
-//             for (let i = 0; i < 11; i++) {
-//                 for (let j = 0; j < 9; j++) {
-//                     k++;
-//                     document.getElementById('showasma').innerHTML += response.data.data[k].name;
-
-//                 }
-//                 document.getElementById('showasma').innerHTML += "<br>";
-//             }
-//         }
-//         )
-// }
-
+        document.getElementById('form').style.display = "none";
+    }
