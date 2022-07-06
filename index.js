ceabebdbc71508688565467ee029707370266bf0
Muhammad Haroon <84071543+26haroon26@users.noreply.github.com>
2022-07-06 22:54:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ function Times() {
 
     axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${inp_ut1.value}&country=${inp_ut2.value}&method=1`)
         .then(function (response) {
-            document.getElementById('fajr').innerHTML = "Fajr :" + " " + response.data.data.timings.Fajr;
-            document.getElementById('sunrise').innerHTML = "Sunrise :" + " " + response.data.data.timings.Sunrise;
-            document.getElementById('zuhur').innerHTML = "Zuhur :" + " " + response.data.data.timings.Dhuhr;
-            document.getElementById('asr').innerHTML = "Asr :" + " " + response.data.data.timings.Asr;
-            document.getElementById('sunset').innerHTML = "Sunset :" + " " + response.data.data.timings.Sunset;
-            document.getElementById('maghrib').innerHTML = "Maghrib" + " " + response.data.data.timings.Maghrib;
-            document.getElementById('esha').innerHTML = "Esha" + " " + response.data.data.timings.Isha;
-            document.getElementById('tahajjud').innerHTML = "Tahajjud :" + " " + response.data.data.timings.Midnight;
-            document.getElementById('hijridate').innerHTML = "Hijri Date :" + " " + response.data.data.date.gregorian.date + " " + response.data.data.date.gregorian.designation.abbreviated;
-            document.getElementById('gregoriandate').innerHTML = "Gregorian Date :" + " " + response.data.data.date.hijri.date + " " + response.data.data.date.hijri.designation.abbreviated;
+            document.getElementById('fajr').innerHTML =  response.data.data.timings.Fajr;
+            document.getElementById('sunrise').innerHTML =  response.data.data.timings.Sunrise;
+            document.getElementById('zuhur').innerHTML =  response.data.data.timings.Dhuhr;
+            document.getElementById('asr').innerHTML =  response.data.data.timings.Asr;
+            document.getElementById('sunset').innerHTML =  response.data.data.timings.Sunset;
+            document.getElementById('maghrib').innerHTML =  response.data.data.timings.Maghrib;
+            document.getElementById('esha').innerHTML =  response.data.data.timings.Isha;
+            document.getElementById('tahajjud').innerHTML = response.data.data.timings.Midnight;
+            document.getElementById('hijridate').innerHTML =  response.data.data.date.gregorian.date + " " + response.data.data.date.gregorian.designation.abbreviated;
+            document.getElementById('gregoriandate').innerHTML =  response.data.data.date.hijri.date + " " + response.data.data.date.hijri.designation.abbreviated;
         }
         )
     inp_ut1.value = "";
