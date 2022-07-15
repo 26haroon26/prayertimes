da60827ee1f3fe821cbd59b32ce9a514c92cf75c
Muhammad Haroon <84071543+26haroon26@users.noreply.github.com>
2022-07-15 22:03:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -192,5 +192,17 @@ function AudioSurah() {
     select.value = "";
     showaudiosurah.value = "";
 }
-
+// function direction() {
+    let maindiv = document.getElementById('main');
+    let qibla_direction = document.getElementById('qiblaimage');
+    axios.get(`http://api.aladhan.com/v1/qibla/25.4106386/51.1846025`)
+            .then(function (response) {
+                console.log(response.data);
+                maindiv.innerHTML = ` <div id="qiblaimage" style="transform: rotate(${response.data.data.direction +"deg"});">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDfwUgPMMXfM3SU6gnjWRL8sxEmXZKqMzeJQ&usqp=CAU">
+            </div>`
+                   
+}
+)
+// }
 
